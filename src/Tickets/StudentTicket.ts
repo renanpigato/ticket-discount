a89486106b27a7112238300e154b12cab63cfa36
Renan Pigato <renan@renanpigato.inf.br>
2022-02-27 15:00:37 -0300

    Corrige identação

--- src/Tickets/StudentTicket.ts
+++ src/Tickets/StudentTicket.ts
@@ -1,26 +1,26 @@
 import Ticket from './Ticket';
 
 class StudentTicket extends Ticket {
-	readonly PRICE: number = 8;
-	readonly DISCOUNT_PERCENT: number = 0.35;
-	readonly WEEKEND: number[] = [6, 0];
+    readonly PRICE: number = 8;
+    readonly DISCOUNT_PERCENT: number = 0.35;
+    readonly WEEKEND: number[] = [6, 0];
 
-	constructor(date: Date)
-	{
-		super(date);
-	}
+    constructor(date: Date)
+    {
+        super(date, Ticket.TYPE_STUDENT);
+    }
 
-	calculate()
-	{
-		const basePrice = super.calculate();
-		let discount = this.DISCOUNT_PERCENT;
+    calculate()
+    {
+        const basePrice = this.PRICE;
+        let discount = this.DISCOUNT_PERCENT;
 
-		if (this.WEEKEND.indexOf(this.date.getDay()) > -1) {
-			discount = 0;
-		}
+        if (this.WEEKEND.indexOf(this.date.getDay()) > -1) {
+            discount = 0;
+        }
 
-		return basePrice - (basePrice * discount)
-	}
+        return basePrice - (basePrice * discount)
+    }
 }
 
 module.exports = StudentTicket;
