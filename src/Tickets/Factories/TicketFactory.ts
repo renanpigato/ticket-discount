f40b431e3f5f6b64f6299ba47cb377120ed66baf
Renan Pigato <renan@renanpigato.inf.br>
2022-02-28 15:07:12 -0300

    Adiciona factory de classes de tickets

---
+++ src/Tickets/Factories/TicketFactory.ts
@@ -0,0 +1,23 @@
+import CalendarDate from '../../CalendarDate/CalendarDate';
+import Ticket from '../Ticket';
+import ChildrenTicket from '../ChildrenTicket';
+import StudentTicket from '../StudentTicket';
+import ElderlyTicket from '../ElderlyTicket';
+
+class TicketFactory {
+    static create(type: string, date: CalendarDate)
+    {
+        switch(type) {
+            case Ticket.TYPE_CHILDREN:
+                return new ChildrenTicket(date);
+            case Ticket.TYPE_STUDENT:
+                return new StudentTicket(date);
+            case Ticket.TYPE_ELDERLY:
+                return new ElderlyTicket(date);
+        }
+
+        return new Ticket(date);
+    }
+}
+
+module.exports = TicketFactory;
