a618f14b354e4f3175171712f06e420d3e97ef50
Renan Pigato <renan@renanpigato.inf.br>
2022-02-28 15:07:49 -0300

    Ajustes após alteraçoes de estrutura de classes

--- src/Tickets/Ticket.ts
+++ src/Tickets/Ticket.ts
@@ -1,25 +1,34 @@
+import CalendarDate from './CalendarDate/CalendarDate';
 import Discount from './Discounts/Discount';
+import DiscountFactory from './Discounts/Factories/DiscountFactory';
 
 class Ticket {
-    readonly TYPE_DEFAULT: string = 'D';
-    readonly TYPE_CHILDREN: string = 'C';
-    readonly TYPE_STUDENT: string = 'S';
-    readonly TYPE_ELDERLY: string = 'E';
-    readonly PRICE: number;
+    static readonly TYPE_DEFAULT: string = 'D';
+    static readonly TYPE_CHILDREN: string = 'C';
+    static readonly TYPE_STUDENT: string = 'S';
+    static readonly TYPE_ELDERLY: string = 'E';
+    static readonly PRICE: number;
 
-    date: Date;
-    typeSelected: string;
+    date: CalendarDate;
+    typeSelected: string = Ticket.TYPE_DEFAULT;
 
-    constructor(date: Date, typeSelected = Ticket.TYPE_DEFAULT)
+    constructor(date: CalendarDate, typeSelected: string | null)
     {
         this.date = date;
-        this.typeSelected = typeSelected;
+
+        if (!!typeSelected) {
+            this.typeSelected = typeSelected;
+        }
     }
 
     calculate(discount: Discount | null = null)
     {
         const price = this.PRICE;
-        const discountCalculate = !!discount ? discount : new Discount(price, this.date, this.typeSelected);
+        const discountCalculate = !!discount ? discount : DiscountFactory.create(this.date, price, this.typeSelected);
+        console.log('Ticket - calculate'
+            ,price
+            ,discountCalculate
+        );
 
         return (price - discountCalculate.calculate());
     }
