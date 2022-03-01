9a3bc6c97dbeaf8f6cddcc7304e35183c4889aa4
Renan Pigato <renan@renanpigato.inf.br>
2022-03-01 12:05:24 -0300

    Altera forma de retorno de desconto zerado

--- src/Tickets/Discounts/DiscountThursday.ts
+++ src/Tickets/Discounts/DiscountThursday.ts
@@ -2,7 +2,6 @@ import Ticket from '../Ticket';
 import Discount from './Discount';
 
 export default class DiscountThursday extends Discount {
-    static readonly DISCOUNT = 0;
     static readonly DISCOUNT_ELDERLY = 0.3;
 
     constructor(basePrice: number, typeTicket: string)
